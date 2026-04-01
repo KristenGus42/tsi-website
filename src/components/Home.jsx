884973033a48674d5b88323253cc411957e24c7d
Kristen <kristengus1@yahoo.com>
2026-04-01 13:46:07 -0700

    Set up partner logos on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,9 @@ export default function Home() {
         <HeaderBanner title="Home" />
         <main>
             <section>
+            <h1 class="mb-4 title-text"> The ExerciseRx.ai Platform </h1>
             <div class="information-section">
-                <h2>The ExerciseRx.ai Platform: How it Works </h2>
+                <h2>How it Works </h2>
                 <p>The ExerciseRxTM platform integrates physical activity into routine medical care. The platform connects providers and patients through a patient-facing mobile app and a provider dashboard embedded in the EPIC electronic health record.</p>
                 <p>Providers recommend personalized exercise and activity for wellness support. Patients track activity on their existing iOS or Android devices. Advanced AI and machine learning monitors their activity through sensors in devices they already own. ExerciseRx helps patients build sustainable healthy movement habits at scale.</p>
                 <p>The platform helps patients manage chronic conditions, prepare for surgery, or increase daily movement. It works on iOS and Android and integrates with standard EHR systems. </p>
@@ -20,8 +21,28 @@ export default function Home() {
             </div>
             {/** TOOD ADD LOGOS */}
             <div class="information-section">
-                <h2>The ExerciseRx platform has been used by: </h2>
-                <p>UW Medical Center, Seattle Children's Hospital, Fred Hutchinson Cancer Research Center, UW MS Rehabilitation and Wellness Center, and Hospital for Special Surgery in New York. </p>
+                <h2> Used By </h2>
+                {/**<p>UW Medical Center, Seattle Children's Hospital, Fred Hutchinson Cancer Research Center, UW MS Rehabilitation and Wellness Center, and Hospital for Special Surgery in New York. </p>*/}
+                {/* Partner Images */}
+                <div clas="container">
+                    <div class="partner-container row row-cols-sm-2 row-cols-md-3">
+                        <div class="partner-container">
+                            <img src="images/partners/seattle_childrens.png"></img>
+                        </div>
+                        <div class="partner-container">
+                            <img src="images/partners/fred-hutch.png"></img>
+                        </div>
+                        <div class="partner-container">
+                            <img src="images/partners/uw-ms-rehab.png"></img>
+                        </div>
+                        <div class="partner-container">
+                            <img src="images/partners/hss.png"></img>
+                        </div>
+                        <div class="partner-container">
+                            <img src="images/partners/uw-med-sq.png"></img>
+                        </div>
+                    </div>
+                </div>
             </div>
             </section>
         </main>
