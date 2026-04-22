d3b4ac9393c0474b15e3d477f1b3c4c0268d3d42
Kristen <kristengus1@yahoo.com>
2026-04-22 00:39:26 -0700

    Changes to sponsor styling and adjusted information on members

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,24 @@ export default function Contact() {
   return (
     <>
         <NavBar />
-        <HeaderBanner title="Contact" />
-
+        <HeaderBanner title="Contact Us" />
+        <main>
+            <div className="d-flex justify-content-evenly">
+                <div>
+                  <h3><i className="fa fa-envelope-o"></i>Email</h3>
+                  <p>exerciserx@uw.edu</p>
+                </div>
+                <div>
+                  <h3><i className="fa fa-map-marker"></i>Mailing Address</h3>
+                  <div className="text-center">
+                    <p className="m-0">The Sports Institute</p>
+                    <p className="m-0">Dept. of Rehabilitation Medicine</p>
+                    <p className="m-0">325 9th Ave, Box 39594</p>
+                    <p className="m-0">Seattle, WA 98104-2499</p>
+                  </div>
+                </div>
+            </div>
+        </main>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,35 +11,59 @@ export default function Home() {
         <HeaderBanner title="Home" />
         <main>
             <section>
-            <h1 class="mb-4 title-text"> The ExerciseRx.ai Platform </h1>
-            <div class="information-section">
+            <h1 className="mb-4 title-text"> The ExerciseRx.ai Platform </h1>
+            <div className="information-section">
                 <h2>How it Works </h2>
                 <p>The ExerciseRxTM platform integrates physical activity into routine medical care. The platform connects providers and patients through a patient-facing mobile app and a provider dashboard embedded in the EPIC electronic health record.</p>
                 <p>Providers recommend personalized exercise and activity for wellness support. Patients track activity on their existing iOS or Android devices. Advanced AI and machine learning monitors their activity through sensors in devices they already own. ExerciseRx helps patients build sustainable healthy movement habits at scale.</p>
                 <p>The platform helps patients manage chronic conditions, prepare for surgery, or increase daily movement. It works on iOS and Android and integrates with standard EHR systems. </p>
                 <p>ExerciseRx is adaptable and works across medical specialties. Current applications include urology, oncology, multiple sclerosis, rheumatology, and primary care. </p>
             </div>
-            {/** TOOD ADD LOGOS */}
-            <div class="information-section">
+            <div className="information-section">
                 <h2> Used By </h2>
                 {/**<p>UW Medical Center, Seattle Children's Hospital, Fred Hutchinson Cancer Research Center, UW MS Rehabilitation and Wellness Center, and Hospital for Special Surgery in New York. </p>*/}
                 {/* Partner Images */}
-                <div clas="container">
-                    <div class="partner-container row row-cols-sm-2 row-cols-md-3">
-                        <div class="partner-container">
-                            <img src="images/partners/seattle_childrens.png"></img>
+                <div className="container">
+                    <div className="partner-container row">
+                        <div className="col-12 col-sm-6 col-md-4 mb-4">
+                            <div className="card">
+                                <img className="card-img-top" src="images/partners/seattle_childrens.png" alt=""/>
+                                <div className="card-body">
+                                    <h4 className="card-text">Seattle Children's</h4>
+                                </div>
+                            </div>
                         </div>
-                        <div class="partner-container">
-                            <img src="images/partners/fred-hutch.png"></img>
+                        <div className="col-12 col-sm-6 col-md-4 mb-4">
+                            <div className="card">
+                                <img className="card-img-top" src="images/partners/fred-hutch.png" alt=""/>
+                                <div className="card-body">
+                                    <h4 className="card-text">Fred Hutch<br/></h4>
+                                </div>
+                            </div>
                         </div>
-                        <div class="partner-container">
-                            <img src="images/partners/uw-ms-rehab.png"></img>
+                        <div className="col-12 col-sm-6 col-md-4 mb-4">
+                            <div className="card">
+                                <img className="card-img-top" src="images/partners/uw-ms-rehab.png" alt=""/>
+                                <div className="card-body">
+                                    <h4 className="card-text">UW MS Rehab<br/></h4>
+                                </div>
+                            </div>
                         </div>
-                        <div class="partner-container">
-                            <img src="images/partners/hss.png"></img>
+                        <div className="col-12 col-sm-6 col-md-4 mb-4">
+                            <div className="card">
+                                <img className="card-img-top" src="images/partners/hss.png" alt=""/>
+                                <div className="card-body">
+                                    <h4 className="card-text">Hospital for Special Surgery</h4>
+                                </div>
+                            </div>
                         </div>
-                        <div class="partner-container">
-                            <img src="images/partners/uw-med-sq.png"></img>
+                        <div className="col-12 col-sm-6 col-md-4 mb-4">
+                            <div className="card">
+                                <img className="card-img-top" src="images/partners/uw-med-sq.png" alt=""/>
+                                <div className="card-body">
+                                    <h4 className="card-text">UW Medicine<br/><br/></h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -50,3 +74,30 @@ export default function Home() {
     </>
   );
 }
+
+/*
+                        <div className="col card">
+                            <img className="card-img-top" src="images/partners/fred-hutch.png" alt=""/>
+                            <div className="card-body">
+                                <h4 className="card-text">Fred Hutch<br/></h4>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img className="card-img-top" src="images/partners/uw-ms-rehab.png" alt=""/>
+                            <div className="card-body">
+                                <h4 className="card-text">UW MS Rehab<br/></h4>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img className="card-img-top" src="images/partners/hss.png" alt=""/>
+                            <div className="card-body">
+                                <h4 className="card-text">Hospital for Special Surgery</h4>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img className="card-img-top" src="images/partners/uw-med-sq.png" alt=""/>
+                            <div className="card-body">
+                                <h4 className="card-text">UW Medidcine<br/><br/></h4>
+                            </div>
+                        </div>
+                        */
